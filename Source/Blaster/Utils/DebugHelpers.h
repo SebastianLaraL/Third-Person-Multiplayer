3f6e04e05f03d98a750d1c43c8cb187ae4547a02
Sebastian L <sebastian.lara6087@alumnos.udg.mx>
2026-06-23 18:01:07 -0600

    new draw capsule macro and reorganized file to allow multiple draw macros

--- Source/Blaster/Utils/DebugHelpers.h
+++ Source/Blaster/Utils/DebugHelpers.h
@@ -1,9 +1,23 @@
 #pragma once
 
-// Macro for drawing debug shapes in development or test configuration and ignore them in Shipping builds. 
+// Macro for drawing debug shapes in development or test configuration. 
+// They are stripped out in Shipping builds.
+
 #if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
+
 #define DRAW_DEBUG_SPHERE(World,Location,Radius,Segments,Color,Persistent) \
-DrawDebugSphere(World, Location, Radius, Segments, Color, Persistent)
+	DrawDebugSphere(World, Location, Radius, Segments, Color, Persistent)
+
+#define DRAW_DEBUG_BOX(World, Center, Box, Rotation, Color, Persistent) \
+	DrawDebugBox(World, Center, Box, Rotation, Color, Persistent)
+
+#define DRAW_DEBUG_CAPSULE(World, Center, HalfHeight, Radius, Rotation, Color, PersistentLines, LifeTime, DepthPriority, Thickness) \
+	DrawDebugCapsule(World, Center, HalfHeight, Radius, Rotation, Color, PersistentLines, LifeTime, DepthPriority, Thickness)
+
 #else
+
 #define DRAW_DEBUG_SPHERE(World,Location,Radius,Segments,Color,Persistent)
+#define DRAW_DEBUG_BOX(World, Center, Box, Rotation, Color, Persistent)
+#define DRAW_DEBUG_CAPSULE(World, Center, HalfHeight, Radius, Rotation, Color, PersistentLines, LifeTime, DepthPriority, Thickness)
+
 #endif
